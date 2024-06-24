b97365aad7a48e18aa2f5debd4a20cc6c708794e
Zarko Stojanov <stojanov1987@gmail.com>
2024-06-24 02:11:30 +0200

    * added router * added login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./Pages/Login";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />}>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
-export default App;
+// 12.6
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,16 @@
+
+
+const Login = () => {
+    return <>
+
+    <form>
+        <input placeholder='korisnicko ime' type='text'/>
+        <input placeholder='unesite lozinku' type='password'/>
+        <button>Login</button>
+
+    </form>
+
+</>
+}
+
+export default Login

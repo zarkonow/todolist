b2ce9247855f13a700542c68dd4866ff11182b0a
zarkonow <stojanov1987@gmail.com>
2024-07-02 23:59:45 +0200

    * last commit

--- src/Partials/Navigation.jsx
+++ src/Partials/Navigation.jsx
@@ -6,10 +6,16 @@ const Navigation = () => {
     const {userState} = useContext(UserContext);
     console.log('userState', userState);
 
+    const logoutUser = () => {
+        localStorage.removeItem('userData')
+        window.location.reload()
+
+    }
+
     return (
         <>
             {userState.isLoggedIn &&
-                <a href='Logout'>Logout</a>
+                <a href='Logout' onClick={logoutUser}>Logout</a>
             }
             {!userState.isLoggedIn &&
                 <a href='Login'>Login</a>
